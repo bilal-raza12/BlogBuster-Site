066b524d1669ab553707df1395c9ddd95f763638
bilal-raza12 <muhammadbilalraza497@gmail.com>
2024-12-31 22:45:29 +0500

    Milestone 3 Assignment

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,8 +6,8 @@ const Footer = () => {
           <p className="text-sm inline-block border-b pb-5">&copy;2021 Blog Buster. All rights reserved.</p>
            <ul className="flex  mt-4 pt-4 mb-5  gap-4">
            <Link href={"/"}><li className="hover:text-blue-500 ">Home</li></Link>
-           <Link href={"/"}><li className="hover:text-blue-500 ">About us</li></Link>
-           <Link href={"/"}><li className="hover:text-blue-500 ">Contact us</li></Link>
+           <Link href={"/about"}><li className="hover:text-blue-500 ">About us</li></Link>
+           <Link href={"/contact"}><li className="hover:text-blue-500 ">Contact us</li></Link>
                
             </ul> 
     </div>
